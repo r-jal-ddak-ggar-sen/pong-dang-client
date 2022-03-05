1b58f506cfd1e340f76c0f9201eff7c309bdc174
kdongha <kdongha1597@gmail.com>
2022-03-05 22:52:50 +0900

    fix: Layout 위치수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,14 @@ import Header from 'components/Header';
 import { Button } from 'components/Button';
 import Modal from 'components/Modal';
 import Typo from 'components/Typo';
+import { Layout } from 'components/Layout';
 
 export default function Home() {
   const [visibleModal, setVisibleModal] = useState(false);
 
   return (
-    <>
-      <HomeStyled>
+    <Layout
+      header={
         <Header
           right={
             <LogoutButton>
@@ -19,21 +20,24 @@ export default function Home() {
             </LogoutButton>
           }
         />
+      }
+    >
+      <>
         <HomeView>
           <Button onClick={() => setVisibleModal(true)}>Modal Open</Button>
         </HomeView>
-      </HomeStyled>
-      {visibleModal && (
-        <Modal
-          title="아직 일기가 저장되지 않았어요."
-          content="저장하지 않고 뒤로 가시겠어요?"
-          prevButtonText="계속쓰기"
-          onPrevButtonClick={() => setVisibleModal(false)}
-          nextButtonText="뒤로가기"
-          onNextButtonClick={() => setVisibleModal(false)}
-        />
-      )}
-    </>
+        {visibleModal && (
+          <Modal
+            title="아직 일기가 저장되지 않았어요."
+            content="저장하지 않고 뒤로 가시겠어요?"
+            prevButtonText="계속쓰기"
+            onPrevButtonClick={() => setVisibleModal(false)}
+            nextButtonText="뒤로가기"
+            onNextButtonClick={() => setVisibleModal(false)}
+          />
+        )}
+      </>
+    </Layout>
   );
 }
 
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
+import Header from 'components/Header';
+import { Layout } from 'components/Layout';
 import React from 'react';
 
 export default function signup() {
-  return <SignupStyled>signup</SignupStyled>;
+  return <Layout header={<Header title='회원가입'/> } >signup</Layout>;
 }
 
 const SignupStyled = styled.div``;
